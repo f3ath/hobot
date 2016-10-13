3caa4ba63893aebe4bdf2aa49152ae248f480e83
Alexey Karapetov <karapetov@gmail.com>
2016-10-13 00:22:34 -0700

    bots are created with api token

--- app/config/service/app.php
+++ app/config/service/app.php
@@ -45,7 +45,7 @@ $app->get('/register/{name}/{password}', function (string $name, string $passwor
 
 
 $app['bot.test'] = function ($app) {
-    $bot = new Hobot($app['config']['bots']['test']['token']);
+    $bot = new Hobot($app['config']['bots']['test']['api_token']);
     $bot->setPostProcessor($app['processor.echo']);
 };
 
